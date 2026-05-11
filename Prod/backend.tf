59731f5b0feee4fe7ffaa9ba2fb4e6f32bf2d939
James Macford Macauley <Jamesmacfordm@gmail.com>
2026-05-10 18:41:01 -0600

    fix: create UAT and Prod state buckets and fix Prod backend region

--- Prod/backend.tf
+++ Prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "bs101-prod-app-buckettt"
     key            = "prod/terraform.tfstate"         # Use a unique path per environment
-    region         = "us-east-2"
+    region         = "af-south-1"
     #dynamodb_table = "bs101-prod-app-dbb"
     encrypt        = true
   }
